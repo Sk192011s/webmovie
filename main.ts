012b709c9707fc6da31f979f516d5b78700b5ff8
Sk192011s <soekyawwin68@gmail.com>
2025-12-26 17:54:03 +0630

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,11 @@
+{
+  "imports": {
+    "hono": "https://deno.land/x/hono@v4.0.9/mod.ts",
+    "hono/": "https://deno.land/x/hono@v4.0.9/",
+    "std/": "https://deno.land/std@0.216.0/"
+  },
+  "compilerOptions": {
+    "jsx": "react-jsx",
+    "jsxImportSource": "hono/jsx"
+  }
+}
