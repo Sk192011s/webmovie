71caeea2bde271a684f02aac725dbf3a4997c5d9
Sk192011s <soekyawwin68@gmail.com>
2025-12-31 02:18:25 +0630

    Update db.ts

--- db.ts
+++ db.ts
@@ -3,13 +3,11 @@ import * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
 
 const kv = await Deno.openKv();
 
-// Auth Helpers
 export const hashPassword = (password: string) => bcrypt.hash(password);
 export const verifyPassword = (password: string, hash: string) => bcrypt.compare(password, hash);
 
-// Database Functions
-export async function getMovies(limit?: number) {
-  const iter = kv.list<Movie>({ prefix: ["movies"] }, limit ? { limit, reverse: true } : { reverse: true });
+export async function getMovies() {
+  const iter = kv.list<Movie>({ prefix: ["movies"] });
   const movies = [];
   for await (const res of iter) movies.push(res.value);
   return movies.sort((a, b) => b.createdAt - a.createdAt);
@@ -18,17 +16,18 @@ export async function getMovies(limit?: number) {
 export async function getMovie(id: string) { return (await kv.get<Movie>(["movies", id])).value; }
 export async function getUser(username: string) { return (await kv.get<User>(["users", username])).value; }
 export async function getKeys() { 
-  const iter = kv.list<VipKey>({ prefix: ["keys"] });
-  const keys = []; for await (const res of iter) keys.push(res.value);
-  return keys;
+    const iter = kv.list<VipKey>({ prefix: ["keys"] });
+    const keys = []; for await (const res of iter) keys.push(res.value);
+    return keys;
 }
 export async function getRequests() {
-  const iter = kv.list<UserRequest>({ prefix: ["requests"] });
-  const reqs = []; for await (const res of iter) reqs.push(res.value);
-  return reqs.sort((a, b) => b.timestamp - a.timestamp);
+    const iter = kv.list<UserRequest>({ prefix: ["requests"] });
+    const reqs = []; for await (const res of iter) reqs.push(res.value);
+    return reqs;
 }
 export async function getConfig() { 
-  return (await kv.get<AppConfig>(["config"])).value || { announcement: "Welcome to Gold Flix!", showAnnouncement: true }; 
+    const res = await kv.get<AppConfig>(["config"]);
+    return res.value || { announcement: "Welcome to Gold Flix!", showAnnouncement: true };
 }
 
 export { kv };
